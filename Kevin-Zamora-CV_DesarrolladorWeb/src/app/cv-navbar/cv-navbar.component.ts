ebe43b2c8deddeb40b2ace0088e776ec5de43815
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-08-05 12:07:57 +0200

    in progress / Adding CV Section Navbar into external component

---
+++ Kevin-Zamora-CV_DesarrolladorWeb/src/app/cv-navbar/cv-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, isStandalone } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-navbar',
+  templateUrl: './cv-navbar.component.html',
+  styleUrls: ['./cv-navbar.component.scss'],
+  standalone: false,
+})
+export class CvNavbarComponent {
+
+}
