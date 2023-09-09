1684be78cf766222c2789ae9fed24bbdeecfb875
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-09 17:47:51 +0200

    in progress / in pause: Building Socialbar to allow sharing CV Resume

--- Kevin-Zamora-CV_DesarrolladorWeb/src/app/cv/cv.page.ts
+++ Kevin-Zamora-CV_DesarrolladorWeb/src/app/cv/cv.page.ts
@@ -9,4 +9,29 @@ export class CvPage {
 
   constructor() {}
 
+  // copyToClipboard() {
+  //   // Get the text field
+  //   var copyText = document.getElementById("email");
+    
+  //   // Select the text field
+  //   copyText.select();
+  //   copyText.setSelectionRange(0, 99999); // For mobile devices
+    
+  //   // Copy the text inside the text field
+  //   navigator.clipboard.writeText(copyText.value);
+    
+  //   // Alert the copied text
+  //   alert("Correo copiado al portapapeles: " + copyText.value);
+  // }
+  
+  // showingSocialbar() {
+  //     console.log('You are into showingSocialbar()');
+  //     var x: any | null = document.getElementById("socialbar");
+  // if (x.style.display === "none") {
+  //     x.style.display = "block";
+  // } else {
+  //     x.style.display = "none";
+  // }
+  // }
+
 }
